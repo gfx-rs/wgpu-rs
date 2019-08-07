428b9df94f81e72a692139cb505c0d33c37cd27d
Layl <LaylConway@users.noreply.github.com>
2019-08-07 20:22:24 +0200

    Fix sampling in mipmap generation example

--- examples/mipmap/main.rs
+++ examples/mipmap/main.rs
@@ -134,7 +134,7 @@ impl Example {
             address_mode_u: wgpu::AddressMode::ClampToEdge,
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Nearest,
+            mag_filter: wgpu::FilterMode::Linear,
             min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Nearest,
             lod_min_clamp: -100.0,
