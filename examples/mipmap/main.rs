45915822903b29c091cc24bf2b040e15f475df9f
Layl <LaylConway@users.noreply.github.com>
2019-08-08 13:43:27 +0200

    Change min_filter in mipmap example to Nearest

--- examples/mipmap/main.rs
+++ examples/mipmap/main.rs
@@ -135,7 +135,7 @@ impl Example {
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
             mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
             mipmap_filter: wgpu::FilterMode::Nearest,
             lod_min_clamp: -100.0,
             lod_max_clamp: 100.0,
