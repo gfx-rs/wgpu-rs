64272bb8bf5e92968bf2effb244f35735b83432b
Aron Granberg <aron.granberg@gmail.com>
2021-03-16 21:35:28 +0100

    Simplify code

--- src/util/device.rs
+++ src/util/device.rs
@@ -42,9 +42,9 @@ impl DeviceExt for crate::Device {
         // Valid vulkan usage is
         // 1. buffer size must be a multiple of COPY_BUFFER_ALIGNMENT.
         // 2. buffer size must be greater than 0.
-        let padded_size = ((unpadded_size + COPY_BUFFER_ALIGNMENT - 1) / COPY_BUFFER_ALIGNMENT)
-            .max(1)
-            * COPY_BUFFER_ALIGNMENT;
+        // Therefore we round the value up to the nearest multiple, and ensure it's at least COPY_BUFFER_ALIGNMENT.
+        let align_mask = COPY_BUFFER_ALIGNMENT - 1;
+        let padded_size = ((unpadded_size + align_mask) & !align_mask).max(COPY_BUFFER_ALIGNMENT);
 
         let wgt_descriptor = crate::BufferDescriptor {
             label: descriptor.label,
